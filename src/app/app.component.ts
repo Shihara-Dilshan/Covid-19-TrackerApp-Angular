6cf6c4cbe00f2a922fa9abdfc22942c34da1a7a3
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-08-24 15:20:14 +0800

    cards implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,46 @@ import M from 'materialize-css';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
-  title:String = 'covid19trackerAngular';
-  allCountries:Array<any>;
+export class AppComponent implements OnInit {
+  public title: String = 'covid19trackerAngular';
+  public allCountries: Array<any>;
+  public currentCountry:string;
+  public currentCountryDetails:object = {};
 
-  async ngOnInit(){
+  async ngOnInit() {
     const elems = document.querySelectorAll('select');
-    M.FormSelect.init(elems, {});
 
-    const apiCall= await fetch('https://disease.sh/v3/covid-19/countries');
+    const apiCall = await fetch('https://disease.sh/v3/covid-19/countries');
     const result = await apiCall.json();
-    this.allCountries = result.map( (detail: any) => {
+    this.allCountries = result.map((detail: any) => {
       return {
         id: detail.countryInfo._id,
         country: detail.country,
         code: detail.countryInfo.iso2,
-      }
+      };
     });
-    console.log(this.allCountries);
-
+    setTimeout(() => {
+      M.FormSelect.init(elems, {});
+    }, 10);
   }
+
+  async selectCountry(event:any){
+    let apiCall:any;
+    let result:any
+    if(event.target.value === 'worldwide'){
+      this.currentCountry = "all";
+      apiCall = await fetch(`https://disease.sh/v3/covid-19/all`);
+      result = await apiCall.json();
+    }else{
+      this.currentCountry = event.target.value;
+      apiCall = await fetch(`https://disease.sh/v3/covid-19/countries/${this.currentCountry}`);
+      result = await apiCall.json();
+    }
+    
+    this.currentCountryDetails = result;
+    console.log(this.currentCountryDetails);
+  }
+
 }
