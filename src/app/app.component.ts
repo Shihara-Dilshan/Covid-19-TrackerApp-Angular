9ebdff92172bd968e33b35a39ee7d77a6b460904
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-08-24 13:41:00 +0800

    API called using fetch, *ngFor problem occcured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'covid19trackerAngular';
+export class AppComponent implements OnInit{
+  title:String = 'covid19trackerAngular';
+  allCountries:Array<any>;
+
+  async ngOnInit(){
+    const elems = document.querySelectorAll('select');
+    M.FormSelect.init(elems, {});
+
+    const apiCall= await fetch('https://disease.sh/v3/covid-19/countries');
+    const result = await apiCall.json();
+    this.allCountries = result.map( (detail: any) => {
+      return {
+        id: detail.countryInfo._id,
+        country: detail.country,
+        code: detail.countryInfo.iso2,
+      }
+    });
+    console.log(this.allCountries);
+
+  }
 }
